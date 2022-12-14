2f720bdfd928982e5303feffada1160d41833420
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2022-12-14 17:39:51 +1100

    Much refactor

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,20 +1,20 @@
-use std::{collections::HashMap, iter::repeat};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+    iter::repeat,
+};
 
-use advent_of_code2022::{print_coordinates, Coordinate};
+use advent_of_code2022::Coordinate;
 use itertools::Itertools;
 use nom::{
     bytes::complete::tag, combinator::map, multi::separated_list1, sequence::separated_pair,
     Finish, IResult,
 };
 
-#[derive(Clone, Debug)]
-struct Input {
-    rock_points: Vec<Coordinate<u32>>,
-}
+type Input = Vec<Coordinate<u32>>;
 
 #[derive(Debug)]
 enum Contents {
-    Empty,
     Rock,
     Sand,
 }
@@ -47,45 +47,28 @@ fn parse_line(input: &str) -> IResult<&str, Vec<Coordinate<u32>>> {
 fn parse_input(input: &str) -> color_eyre::Result<Vec<Input>> {
     Ok(input
         .lines()
-        .map(|line| Input {
-            rock_points: parse_line(line).finish().unwrap().1,
-        })
+        .map(|line| parse_line(line).finish().unwrap().1)
         .collect())
 }
 
 fn iterate_points(start: &Coordinate<u32>, end: &Coordinate<u32>) -> Vec<Coordinate<u32>> {
     if start.row == end.row {
-        // make sure they're the same order
-        if start.col < end.col {
-            (start.col..=end.col)
-                .zip(repeat(start.row))
-                .map(|(col, row)| Coordinate { row, col })
-                .collect_vec()
-        } else {
-            (end.col..=start.col)
-                .zip(repeat(start.row))
-                .map(|(col, row)| Coordinate { row, col })
-                .collect_vec()
-        }
+        (min(start.col, end.col)..=(max(start.col, end.col)))
+            .zip(repeat(start.row))
+            .map(|(col, row)| Coordinate { row, col })
+            .collect_vec()
     } else {
-        if start.row < end.row {
-            (start.row..=end.row)
-                .zip(repeat(start.col))
-                .map(|(row, col)| Coordinate { row, col })
-                .collect_vec()
-        } else {
-            (end.row..=start.row)
-                .zip(repeat(start.col))
-                .map(|(row, col)| Coordinate { row, col })
-                .collect_vec()
-        }
+        (min(start.row, end.row)..=(max(start.row, end.row)))
+            .zip(repeat(start.col))
+            .map(|(row, col)| Coordinate { row, col })
+            .collect_vec()
     }
 }
 
 fn build_grid(input: &Vec<Input>) -> HashMap<Coordinate<u32>, Contents> {
     let mut map = HashMap::new();
     for line in input {
-        for (start, end) in line.rock_points.iter().tuple_windows() {
+        for (start, end) in line.iter().tuple_windows() {
             // every point between the points is rock
             let coords = iterate_points(start, end);
             for c in coords {
@@ -96,19 +79,6 @@ fn build_grid(input: &Vec<Input>) -> HashMap<Coordinate<u32>, Contents> {
     map
 }
 
-fn access_grid<'a>(
-    coordinate: &Coordinate<u32>,
-    grid: &'a HashMap<Coordinate<u32>, Contents>,
-    lowest: u32,
-    part: u32,
-) -> Option<&'a Contents> {
-    if part == 2 && coordinate.row == lowest + 2 {
-        Some(&Contents::Rock)
-    } else {
-        grid.get(coordinate)
-    }
-}
-
 fn drop_sand(
     grid: &HashMap<Coordinate<u32>, Contents>,
     lowest: u32,
@@ -117,10 +87,9 @@ fn drop_sand(
     let mut current = SAND_ORIGIN;
     loop {
         if part == 1 && current.row > lowest {
-            dbg!(&current);
             return None;
         }
-        let contents = access_grid(&current, grid, lowest, part);
+        let contents = grid.get(&current);
         match contents {
             Some(_) => {
                 // there's something in the way
@@ -128,7 +97,7 @@ fn drop_sand(
                     row: current.row,
                     col: current.col - 1,
                 };
-                let left = access_grid(&left_coord, grid, lowest, part);
+                let left = grid.get(&left_coord);
                 if left.is_none() {
                     current = left_coord;
                 } else {
@@ -136,7 +105,7 @@ fn drop_sand(
                         row: current.row,
                         col: current.col + 1,
                     };
-                    let right = access_grid(&right_coord, grid, lowest, part);
+                    let right = grid.get(&right_coord);
                     if right.is_none() {
                         current = right_coord;
                     } else {
@@ -160,13 +129,15 @@ fn drop_sand(
 
 fn solve_part1(input: &Vec<Input>) -> usize {
     let mut grid = build_grid(input);
-    // print_coordinates(&grid.keys().copied().collect_vec(), true);
     // if we go below this, we're off the map
     let lowest = grid.keys().map(|c| c.row).max().unwrap();
     while let Some(c) = drop_sand(&grid, lowest, 1) {
         grid.insert(c, Contents::Sand);
-        // print_coordinates(&grid.keys().copied().collect_vec(), true);
     }
+    count_grains(&grid)
+}
+
+fn count_grains(grid: &HashMap<Coordinate<u32>, Contents>) -> usize {
     grid.iter()
         .filter(|(_, v)| matches!(v, Contents::Sand))
         .count()
@@ -174,19 +145,22 @@ fn solve_part1(input: &Vec<Input>) -> usize {
 
 fn solve_part2(input: &Vec<Input>) -> usize {
     let mut grid = build_grid(input);
-    // print_coordinates(&grid.keys().copied().collect_vec(), true);
     // if we go below this, we're off the map
     let lowest = grid.keys().map(|c| c.row).max().unwrap();
+    let floor = lowest + 2;
+    // widest shape is a triangle
+    let width = 2 * floor + 1;
+    for col in (SAND_ORIGIN.col - width / 2)..=(SAND_ORIGIN.col + width / 2) {
+        grid.insert(Coordinate { row: floor, col }, Contents::Rock);
+    }
+
     while let Some(c) = drop_sand(&grid, lowest, 2) {
         grid.insert(c, Contents::Sand);
         if c == SAND_ORIGIN {
             break;
         }
-        // print_coordinates(&grid.keys().copied().collect_vec(), true);
     }
-    grid.iter()
-        .filter(|(_, v)| matches!(v, Contents::Sand))
-        .count()
+    count_grains(&grid)
 }
 
 #[cfg(test)]
