d1c2793809ec37aab6593552a369be6a592a0279
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2022-12-21 21:47:42 +1100

    Print an equation. We don't solve it, but it's just math now

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,10 +1,7 @@
 use std::{
-    cmp::Ordering::{Equal, Greater, Less},
     collections::HashMap,
-    time::Instant,
+    time::Instant, fmt::Display,
 };
-
-use num::abs;
 
 #[derive(Debug, Clone)]
 pub struct Input<'a> {
@@ -16,21 +13,49 @@ pub struct Input<'a> {
 pub enum Value<'a> {
     Constant(i64),
     Equation(Operation<'a>),
+    Name(&'a str)
+}
+
+impl <'a> Display for Value<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Constant(x) => write!(f, "{}", x),
+            Value::Equation(e) => write!(f, "({})", e),
+            Value::Name(name) => write!(f, "{}", name),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 pub struct Operation<'a> {
-    left: &'a str,
+    left: Box<Value<'a>>,
     operator: Operator,
-    right: &'a str,
+    right: Box<Value<'a>>,
 }
 
-#[derive(Debug, Clone)]
+impl <'a> Display for Operation<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.left, self.operator, self.right)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum Operator {
     Add,
     Multiply,
     Subtract,
     Divide,
+}
+
+impl Display for Operator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Operator::Add => " + ",
+            Operator::Multiply => " * ",
+            Operator::Subtract => " - ",
+            Operator::Divide => " / ",
+        })
+    }
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -88,9 +113,9 @@ mod parsing {
         map(
             tuple((take(4usize), parse_operator, take(4usize))),
             |(left, operator, right)| Operation {
-                left,
+                left: Box::new(Value::Name(left)),
                 operator,
-                right,
+                right: Box::new(Value::Name(right)),
             },
         )(input)
     }
@@ -122,34 +147,51 @@ fn solve_part1(input: &[Input]) -> i64 {
         .iter()
         .map(|monkey| (monkey.name, monkey.value.clone()))
         .collect();
-    let human_value = if let Value::Constant(x) = monkey_values.get("humn").unwrap() {
+    if let Value::Constant(x) = find_value_for_monkey("root", &monkey_values, 1).unwrap() {
         x
     } else {
-        panic!("Invalid value");
-    };
-    find_value_for_monkey("root", &monkey_values, *human_value).unwrap()
+        panic!("Invalid result")
+    }
 }
 
-fn find_value_for_monkey(
+fn find_value_for_monkey<'a>(
     name: &str,
     monkeys: &HashMap<&str, Value>,
-    human_value: i64,
-) -> Option<i64> {
-    if name == "humn" {
-        return Some(human_value);
+    part: u32
+) -> Option<Value<'a>> {
+    if name == "humn" && part == 2 {
+        return Some(Value::Name("humn"));
     }
     match monkeys.get(name).unwrap() {
-        Value::Constant(x) => Some(*x),
+        Value::Constant(x) => Some(Value::Constant(*x)),
         Value::Equation(operation) => {
-            let left = find_value_for_monkey(operation.left, monkeys, human_value)?;
-            let right = find_value_for_monkey(operation.right, monkeys, human_value)?;
-            match operation.operator {
-                Operator::Add => left.checked_add(right),
-                Operator::Multiply => left.checked_mul(right),
-                Operator::Subtract => left.checked_sub(right),
-                Operator::Divide => left.checked_div(right),
+            if let Value::Name(left_name) = operation.left.as_ref() {
+                if let Value::Name(right_name) = operation.right.as_ref() {
+                    let left = find_value_for_monkey(left_name, monkeys, part)?;
+                    let right = find_value_for_monkey(right_name, monkeys, part)?;
+                    match (left, right) {
+                        (Value::Constant(l), Value::Constant(r)) => {
+                            match operation.operator {
+                                Operator::Add => l.checked_add(r),
+                                Operator::Multiply => l.checked_mul(r),
+                                Operator::Subtract => l.checked_sub(r),
+                                Operator::Divide => l.checked_div(r),
+                            }.map(|result| Value::Constant(result))
+                        },
+                        (value_left, value_right) => Some(Value::Equation(Operation {
+                            left: Box::new(value_left),
+                            operator: operation.operator,
+                            right: Box::new(value_right)
+                        }))
+                    }
+                } else {
+                    unreachable!("Not a valid input")
+                }
+            } else {
+                unreachable!("Not a valid input")
             }
         }
+        _ => unreachable!("There are no names in the input")
     }
 }
 
@@ -158,122 +200,9 @@ fn solve_part2(input: &[Input]) -> Option<i64> {
         .iter()
         .map(|monkey| (monkey.name, monkey.value.clone()))
         .collect();
-    let human_value = if let Value::Constant(x) = monkey_values.get("humn").unwrap() {
-        *x
-    } else {
-        panic!("Invalid value");
-    };
-    let root = monkey_values.get("root").unwrap();
-    let left_child: &str;
-    let right_child: &str;
-    let left_value: i64;
-    let right_value: i64;
-    let is_left = match root {
-        Value::Constant(_) => unreachable!("This never happens"),
-        Value::Equation(operation) => {
-            left_child = operation.left;
-            right_child = operation.right;
-            println!(
-                "Test output is {} and {}",
-                find_value_for_monkey(operation.left, &monkey_values, 3555057453229)?,
-                find_value_for_monkey(operation.right, &monkey_values, 3555057453231)?
-            );
-            left_value = find_value_for_monkey(operation.left, &monkey_values, human_value)?;
-            right_value = find_value_for_monkey(operation.right, &monkey_values, human_value)?;
-            let modified_left =
-                find_value_for_monkey(operation.left, &monkey_values, human_value - 100)?;
-            let modified_right =
-                find_value_for_monkey(operation.right, &monkey_values, human_value - 100)?;
-            dbg!(left_value, modified_left, right_value, modified_right);
-            if left_value != modified_left {
-                Some(true)
-            } else if right_value != modified_right {
-                Some(false)
-            } else {
-                None
-            }
-        }
-    };
-    assert!(is_left.is_some());
-    match is_left {
-        Some(left) => {
-            if left {
-                Some(binary_search(left_child, &monkey_values, right_value))
-            } else {
-                Some(binary_search(right_child, &monkey_values, left_value))
-            }
-        }
-        None => panic!("both sides are on the critical path"),
-    }
-}
-
-fn binary_search(name: &str, monkeys: &HashMap<&str, Value>, target: i64) -> i64 {
-    dbg!(target);
-    // numbers found experimentally
-    let mut min = -100_000_000_000_000;
-    let mut max = 100_000_000_000_000;
-    let mut human_value = 0;
-    let output_base = find_value_for_monkey(name, monkeys, human_value).unwrap();
-    let output_increasing = find_value_for_monkey(name, monkeys, 100_000_000_000_000).unwrap();
-    let output_decreasing = find_value_for_monkey(name, monkeys, -100_000_000_000_000).unwrap();
-    let (increasing_from_zero, positive_correlation) = match (
-        output_increasing.cmp(&target),
-        output_base.cmp(&target),
-        output_decreasing.cmp(&target),
-    ) {
-        (Greater, Greater, Less) => (false, true),
-        (Greater, Less, Less) => (true, true),
-        (Less, Less, Greater) => (false, false),
-        (Less, Greater, Greater) => (true, false),
-        (Equal, _, _) => {
-            return max / 4;
-        }
-        (_, Equal, _) => {
-            return human_value;
-        }
-        (_, _, Equal) => {
-            return min / 4;
-        }
-        comparisons => panic!("Found {:?}", comparisons),
-    };
-    dbg!(increasing_from_zero);
-    if increasing_from_zero {
-        min = 0;
-    } else {
-        max = 0;
-    }
-    let mut output = Some(output_base);
-    while output != Some(target) {
-        match (increasing_from_zero, positive_correlation) {
-            (true, true) => {
-                if output.is_none() {
-                    max = human_value;
-                } else if output < Some(target) {
-                    min = human_value;
-                } else {
-                    max = human_value;
-                }
-            }
-            (true, false) => {
-                if output.is_none() || output < Some(target) {
-                    // we overflowed
-                    max = human_value;
-                } else {
-                    min = human_value;
-                }
-            }
-            (false, true) => todo!(),
-            (false, false) => todo!(),
-        }
-        // dbg!(min, max);
-        if human_value == (min + max) / 2 {
-            panic!("Infinite looping");
-        }
-        human_value = (min + max) / 2;
-        output = find_value_for_monkey(name, monkeys, human_value);
-        // dbg!(output);
-    }
-    human_value
+    let end_value = find_value_for_monkey("root", &monkey_values, 2);
+    println!("{}", end_value.unwrap());
+    Some(1)
 }
 
 #[cfg(test)]
